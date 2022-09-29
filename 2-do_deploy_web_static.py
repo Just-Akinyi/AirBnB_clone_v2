fa38684d36a70feb02da14bb8ee58ed95e65277b
Just-Akinyi <justakinyi1@gmail.com>
2022-09-29 21:51:20 +0300

    Write a Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-This script distributes an archive to your web servers, using the function
-do_deploy
-"""
-from fabric.api import put, run, env
+""" Does deployment"""
+
+from fabric.api import *
 import os
 
 env.hosts = ['3.237.38.31', '34.204.193.193']
@@ -11,36 +9,26 @@ env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    deploys archive to web server
-    """
-    path = archive_path.split('/')[-1]
-    line = path.split('.')[0]
-    if not os.path.isfile(archive_path):
+    """ Deploys archive to servers"""
+    if not os.path.exists(archive_path):
         return False
-    if put(archive_path, "/tmp/{}".format(path)).failed:
-        return False
-    if run("rm -rf /data/web_static/releases/{}"
-            .format(line)).failed:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/"
-            .format(line)).failed:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(path, line)).failed:
-        return False
-    if run("rm /tmp/{}".format(path)).failed:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(line, line)).failed:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(line)).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(line)).failed:
-        return False
+
+    results = []
+
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
+
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("mkdir -p " + newdir)
+    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+
+    run("rm /tmp/" + basename)
+    run("mv " + newdir + "/web_static/* " + newdir)
+    run("rm -rf " + newdir + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + newdir + " /data/web_static/current")
 
     return True
